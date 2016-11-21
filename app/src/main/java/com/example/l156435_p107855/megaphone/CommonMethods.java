8a0458d8021d2abca035210d21a33400a080f4e2
luis15 <coelho.luisantonio@hotmail.com>
2016-11-21 19:46:03 -0200

    PT 1

---
+++ app/src/main/java/com/example/l156435_p107855/megaphone/CommonMethods.java
@@ -0,0 +1,28 @@
+package com.example.l156435_p107855.megaphone;
+
+/**
+ * Created by coelh on 21/11/2016.
+ */
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CommonMethods {
+    private static DateFormat dateFormat = new SimpleDateFormat("d MMM yyyy");
+    private static DateFormat timeFormat = new SimpleDateFormat("K:mma");
+
+    public static String getCurrentTime() {
+
+        Date today = Calendar.getInstance().getTime();
+        return timeFormat.format(today);
+    }
+
+    public static String getCurrentDate() {
+
+        Date today = Calendar.getInstance().getTime();
+        return dateFormat.format(today);
+    }
+
+}
